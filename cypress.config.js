9c6d07daf8081b644c0667b1c6a23bea242a67ff
Francisco Soares da Silva Neto <francisco.soaresnto@gmail.com>
2025-03-16 19:50:27 -0300

    adesão de Report

--- cypress.config.js
+++ cypress.config.js
@@ -8,5 +8,14 @@ module.exports = defineConfig({
       
     },
     video: true,
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/reports',
+      overwrite: false,
+      html: true,
+      json: true,
+      timestamp: 'mmddyyyy_HHMMss',
+    },
   },
+
 });
